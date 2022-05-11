d736f1add71948e8d135b7842b83f4b9f74b285b
koshiroanz <cristiananzawa@gmail.com>
2022-05-11 11:53:45 -0300

    init commit

---
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -0,0 +1,53 @@
+package com.bolsadeideas.springboot.form.app.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
+import com.bolsadeideas.springboot.form.app.validation.UsuarioValidador;
+
+@Controller
+@SessionAttributes("usuario")
+public class FormController {
+	@Autowired
+	private UsuarioValidador validador;
+	
+	@InitBinder public void initBinder(WebDataBinder binder) {
+		binder.addValidators(validador); 
+	}
+	 
+	@GetMapping("/form")
+	public String form(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setId("12.456.887-K");
+		
+		model.addAttribute("titulo", "Formulario de Usuario");
+		model.addAttribute("usuario", usuario);
+		
+		return "form";
+	}
+	
+	@PostMapping("/form")
+	public String process(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status) {
+		model.addAttribute("titulo", "Formulario de Usuario");
+		
+		if(result.hasErrors())
+			return "form";
+		
+		model.addAttribute("titulo", "Resultado de Usuario");
+		model.addAttribute("usuario", usuario);
+		status.setComplete();
+		
+		return "result";
+	}
+}
---
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -0,0 +1,107 @@
+package com.bolsadeideas.springboot.form.app.models.domain;
+
+import java.util.Date;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.bolsadeideas.springboot.form.app.validation.Requerido;
+
+public class Usuario {
+	private String id;
+	
+	@Requerido
+	private String nombre;
+	
+	@Requerido
+	private String apellido;
+	
+	@NotNull
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private Date dob;
+	
+	@Size(min=3, max=8)
+	@Requerido
+	private String username;
+	
+	@Email
+	@Requerido
+	private String email;
+	
+	@Requerido
+	private String password;
+	
+	@NotNull
+	@Min(5)
+	@Max(5000)
+	private Integer cuenta;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getCuenta() {
+		return cuenta;
+	}
+
+	public void setCuenta(Integer cuenta) {
+		this.cuenta = cuenta;
+	}
+}
