127d2e8101f42d76bbe6b4b07af9dcfe54eb0600
koanz <cristiananzawa@gmail.com>
2022-05-11 12:37:08 -0300

    se elimina import de la anotacion @Past

--- src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -7,7 +7,6 @@ import javax.validation.constraints.Future;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
