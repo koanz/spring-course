5b8cba444d3730def06bc54cfac16e070e99190f
koanz <cristiananzawa@gmail.com>
2022-05-11 15:20:16 -0300

    se comenta la anotacion @Requerido en atributo nombre, y se modifica la anotacion en atributo dob a @Past

--- src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -3,28 +3,28 @@ package com.bolsadeideas.springboot.form.app.models.domain;
 import java.util.Date;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Future;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
-import org.springframework.format.annotation.DateTimeFormat;
+//import org.springframework.format.annotation.DateTimeFormat;
 
 import com.bolsadeideas.springboot.form.app.validation.Requerido;
 
 public class Usuario {
 	private String id;
 	
-	@Requerido
+	//@Requerido
 	private String nombre;
 	
 	@Requerido
 	private String apellido;
 	
 	@NotNull
-	@Future
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@Past
+	//@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date dob;
 	
 	@Size(min=3, max=8)
