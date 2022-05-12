a444f0dfdc4e072dbbf63a09548fe27e350edc18
koanz <cristiananzawa@gmail.com>
2022-05-12 20:37:01 -0300

    Se elimina un espacio en blanco

--- src/main/java/com/bolsadeideas/springboot/form/app/validation/RequeridoValidador.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/validation/RequeridoValidador.java
@@ -10,7 +10,6 @@ public class RequeridoValidador implements ConstraintValidator<Requerido, String
 
 	@Override
 	public boolean isValid(String value, ConstraintValidatorContext context) {
-		//if(value == null || value.isEmpty() || value.isBlank())
 		if(value == null || !StringUtils.hasText(value))
 			return false;
 		
